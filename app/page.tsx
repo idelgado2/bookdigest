5f2cd8ca9ca05390cafbb29a06aad8acecf8d1db
Isaac Delgado <isaacdelgado1994@gmail.com>
2025-01-19 01:57:26 -0800

    adding basic dynamic form

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const link = [{ name: "StartNotes", href: "/notebook" }];
 
 export default function Home() {
+  const pathname = usePathname();
   return (
     <>
-      <div
-        className="
-        grid
-        grid-rows-[20px_1fr_20px]
-        gap-16
-        min-h-screen
-        sm:p-20
-        p-8
-        pb-20
-        items-center
-        justify-items-center
-        font-[family-name:var(--font-geist-sans)]
-        "
-      >
+      <div className="grid grid-rows-[20px_1fr_20px] gap-16 min-h-screen sm:p-20 p-8 pb-20 items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <Button className="delay-150 ease-in-out hover:-translate-y-1 hover:scale-110">
-              Start Book Notes!
-            </Button>
+            <Link key={link[0].name} href={link[0].href}>
+              <Button className="delay-150 ease-in-out hover:-translate-y-1 hover:scale-110">
+                Start Book Notes!
+              </Button>
+            </Link>
           </div>
         </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
